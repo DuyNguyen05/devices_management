b37ca35bcd220125e14022597e07a4bb66845917
su9arfree <tompham3385@gmail.com>
2019-08-20 09:03:10 +0700

    create login-logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by email: params[:session][:email]
+    if User && user.authenticate(params[:session][:password])
+      flash[:success] = "Welcome"
+      log_in(user)
+      redirect_to user
+    else
+      flash[:danger] = "Invalid email/password"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    flash[:success] = "Hope to see you again"
+    redirect_to login_path
+  end
+end
