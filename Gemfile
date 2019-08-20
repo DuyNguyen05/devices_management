1e08749df779a2d0a5eb8a2ceb2bf6422f4b4316
Duy Nguyen <nguyenngocduy.84@live.com>
2019-08-20 08:52:31 +0700

    details-device

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem "faker"
 gem "haml-rails"
 gem "jquery-rails"
 gem "bcrypt"
+gem "jquery-ui-rails"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,18 +1,48 @@
 class DevicesController < ApplicationController
 
   def index
-    @devices = Device.all
+    if params[:keyword].present?
+      @devices = Device.includes(:requests).search_device_by_name_or_code params[:keyword]
+    else
+      @devices = Device.includes(:requests)
+    end
   end
 
   def new
-
+    @device = Device.new
+    respond_to do |format|
+      format.js
+    end
   end
 
   def create
-
+    @device = Device.new device_params
+    if @device.save
+      @devices = Device.includes(:requests).order("created_at desc")
+      respond_to do |format|
+        format.js
+      end
+    else
+      render :new
+    end
   end
 
   def show
+    @device = Device.includes(:requests).find params[:id]
+  end
 
+  def destroy
+    @device = Device.find params[:id]
+    @device.destroy
+    @devices = Device.includes(:requests)
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def device_params
+    params.require(:device).permit :name, :code
   end
 end
