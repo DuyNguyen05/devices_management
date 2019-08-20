b30e1f279683ecfdfa59cd7522480662a1f06a5a
su9arfree <tompham3385@gmail.com>
2019-08-20 13:54:41 +0700

    delete coffee script

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,30 @@
 class UsersController < ApplicationController
 
-    def index
-        @user = User.all
-    end
+  def index
+    @user = User.all
+  end
 
-    def new
-        @user = User.new
-    end
+  def new
+    @user = User.new
+  end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            flash[:success] = "Register Success"
-            redirect_to users_path
-        else
-            flash[:danger] = "Fail"
-            render :new
-        end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Register Success"
+      redirect_to users_path
+    else
+      flash[:danger] = "Fail"
+      render :new
     end
+  end
 
-    def show
-        @user = User.find_by id: params[:id]
-    end
+  def show
+    @user = User.find_by id: params[:id]
+  end
     
-    private
-    def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
