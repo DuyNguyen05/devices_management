83b4efa69cbbcca90d49f3f9c9bfc5b9d5ca6cd5
su9arfree <tompham3385@gmail.com>
2019-08-23 09:16:40 +0700

    change space

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   has_secure_password
 
   def self.match_name_email(querry)
-		if querry
-			where("name LIKE ? OR email LIKE ? ", "%#{querry}%", "%#{querry}%")
-		else
-			where(nil)
-		end	
-	end
+    if querry
+      where("name LIKE ? OR email LIKE ? ", "%#{querry}%", "%#{querry}%")
+    else
+      where(nil)
+    end
+  end
 end
