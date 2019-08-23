999107e3988fa4f35a2e1b0353cf0cdc155b49b7
Duy Nguyen <nguyenngocduy.84@live.com>
2019-08-23 14:06:37 +0700

    Pagination for devices

--- app/models/device.rb
+++ app/models/device.rb
@@ -7,4 +7,22 @@ class Device < ApplicationRecord
 
   validates :name, presence: true
   validates :code, presence: true, uniqueness: true
+
+  class << self
+    def per_page
+      10
+    end
+
+    def total_page(per_page =  self.per_page)
+      pages = (count / per_page.to_f).ceil
+    end
+
+    def paginate(page, per_page: self.per_page)
+      page = page.to_i
+      per_page = per_page.to_i
+
+      offset = (page-1)  * per_page
+      limit(per_page).offset(offset)
+    end
+  end
 end
