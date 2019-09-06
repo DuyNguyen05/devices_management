ff9ad589ea71f965dc2dcabe51a9b73f469c27fa
su9arfree <tompham3385@gmail.com>
2019-09-06 13:57:35 +0700

    add kaminari for device

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     if params[:query].present?
       @users = User.match_name_email(params[:query]).page(params[:page])
     else
-      @users = User.all.page(params[:page]).per(2)
+      @users = User.all.page(params[:page]).per(10)
     end
   end
 
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      @users = User.page(params[:page]).per(10)
       flash[:success] = "Register Success"
       redirect_to users_path
     else
@@ -31,6 +32,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
+    @users = User.page(params[:page]).per(10)
     respond_to do |format|
       format.html { redirect_to users_url, notice: "User was successfully destroyed." }
       format.json { head :no_content }
