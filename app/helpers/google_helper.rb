3837eb47065982d5b62e251e97ef07678ab8b7be
su9arfree <tompham3385@gmail.com>
2019-09-06 08:34:42 +0700

    add httparty

--- app/helpers/google_helper.rb
+++ app/helpers/google_helper.rb
@@ -22,17 +22,14 @@ module GoogleHelper
     params['grant_type'] = 'authorization_code'
 
     # Initialize HTTP library
-    url = URI.parse('https://accounts.google.com')
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE 
-
+    url = HTTParty.get('https://accounts.google.com').to_json
+  
     # Make request for tokens
-    request = Net::HTTP::Post.new('/o/oauth2/token')
-    request.set_form_data({code: code, client_id: ENV['GOOGLE_CLIENT_ID'], client_secret: ENV['GOOGLE_CLIENT_SECRET'], redirect_uri: ENV['BASEURL'] + "oauth2callback", grant_type: "authorization_code" })
-    response = http.request(request)
-
-    JSON.parse(response.body)
+    request = HTTParty.post('https://accounts.google.com/o/oauth2/token', 
+                :body => {code: code, client_id: ENV['GOOGLE_CLIENT_ID'], client_secret: ENV['GOOGLE_CLIENT_SECRET'], redirect_uri: ENV['BASEURL'] + "oauth2callback", grant_type: "authorization_code" }.to_json,
+                :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'})    
+    return request
+    
 
   end
 
@@ -46,18 +43,14 @@ module GoogleHelper
     params['grant_type'] = 'refresh_token'
 
     # Initialize HTTP library
-    url = URI.parse('https://accounts.google.com')
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE 
+    url = HTTParty.get('https://accounts.google.com').to_json
 
     # Make request for tokens
-    request = Net::HTTP::Post.new('/o/oauth2/token')
-    request.set_form_data(params)
-    response = http.request(request)
-
+    request = HTTParty.post('https://accounts.google.com/o/oauth2/token', 
+      :body => {code: code, client_id: ENV['GOOGLE_CLIENT_ID'], client_secret: ENV['GOOGLE_CLIENT_SECRET'], redirect_uri: ENV['BASEURL'] + "oauth2callback", grant_type: "authorization_code" }.to_json,
+      :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'})
     # Parse the response
-    user_tokens = JSON.parse(response.body)
+    user_tokens = request
 
     # Save the new access_token to the user
     user.access_token = user_tokens['access_token']
@@ -74,41 +67,31 @@ module GoogleHelper
     path = '/oauth2/v1/tokeninfo'
 
     # Initialize HTTP library
-    url = URI.parse('https://www.googleapis.com')
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE 
+    url = HTTParty.get('https://accounts.google.com/oauth2/v1/tokeninfo').to_json
 
     # Make request to API
-    request = Net::HTTP::Get.new(path)
-    request['Authorization'] = 'Bearer ' + access_token
-    response = http.request(request)
+    request = HTTParty.get('https://accounts.google.com/oauth2/v1/tokeninfo', 
+              :headers => {'Authorization': 'Bearer' + access_token})
 
-    result = JSON.parse(response.body)
+    return requests
 
     if(result['error'] != nil && result['error'] == 'invalid_token')
       false
     else
       true
     end
-
   end
 
   def call_api(path, access_token)
 
     # Initialize HTTP library
-    url = URI.parse('https://www.googleapis.com')
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE 
+    url = HTTParty.get('https://www.googleapis.com').to_json
 
     # Make request to API
-    request = Net::HTTP::Get.new(path)
-    request['Authorization'] = 'Bearer ' + access_token
-    response = http.request(request)
-
-    JSON.parse(response.body)
-
+    
+    request = HTTParty.get('https://www.googleapis.com' + path, 
+              :headers => {'Authorization': 'Bearer' + access_token})    
+    return request
   end
 
 end
