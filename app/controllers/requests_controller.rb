91edb64cea2d9df8c7e16ad1885e2e2c89a56ca8
Duy Nguyen <nguyenngocduy.84@live.com>
2019-09-11 11:37:27 +0700

    show_modal_assgin

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -4,19 +4,23 @@ class RequestsController < ApplicationController
 
   def new
     @request = Request.new
+    @device = Device.find params[:device_id]
     if params[:query].present?
       @users = User.match_name_email(params[:query]).page(params[:page]).per(10)
     else
       @users = User.all.page(params[:page]).per(10)
+    end
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
   def create
     @request = Request.new request_params
     if @request.save
-      @devices = Device.includes(:requests).page(params[:page]).per(10)
       respond_to do |format|
-        format.js
+        format.js{ flash[:success] = "Assign User To Device Success"}
       end
     else
       render "new"
